aa2b56b1da19c37b225465410cb49575408fe8f3
Brian <chillandpepers2k@gmail.com>
2024-04-19 08:26:56 -0600

    cart-start

--- app/(protected)/home/cart/page.jsx
+++ app/(protected)/home/cart/page.jsx
@@ -1,6 +1,11 @@
 import IconBox from "@/components/ui/IconBox";
 import Link from "next/link";
 import { MdOutlineArrowBack } from "react-icons/md";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/Loader";
+
+
+
 
 export default function CartPage() {
     return (
@@ -9,7 +14,16 @@ export default function CartPage() {
                     <Link href={"/home"}>
                         <IconBox variant={"square"} Icon={MdOutlineArrowBack} />
                     </Link>
-                <h1 className="font-bold text-2xl">Mi Carrito</h1>
+                <h1 className="font-bold text-2xl">Carrito de compras </h1>
+
+                <div className="">
+                    <h1 className="font-bold text-2xl">    El Chory </h1>
+                    <p>Chory Original</p>
+                    <h1>$1.50
+                    </h1>
+                </div>
+
+
             </div>
         </main>
     )
